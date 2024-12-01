7ef1d8d6d56f28cf76aa8218ebc215b8cca64305
Ben Mitchell <editorwroxton@gmail.com>
2024-12-01 18:28:13 +0000

    Changes of the day

---
+++ JS_SnowEffect.js
@@ -0,0 +1,7 @@
+<script>
+    // Customize the snow effect
+    SnowTheme.config.density = 200;    
+    SnowTheme.config.minDuration = 20;  
+    SnowTheme.config.maxSize = 1.5;    
+    SnowTheme.config.wind = 20;       
+</script>
